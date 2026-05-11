a1981b5ffa1cd69d35e499cb0cec0029bc82fadf
Zhao An <zhao.an@amd.com>
2026-05-11 08:17:16 +0000

    refactor: remove dump and refactor attention_backend

--- atom/plugin/rtpllm/model_ops/__init__.py
+++
@@ -1,4 +0,0 @@
-from .attention_gdn import apply_attention_gdn_rtpllm_patch
-from .attention_switch import apply_attention_mha_rtpllm_patch
-
-__all__ = ["apply_attention_gdn_rtpllm_patch", "apply_attention_mha_rtpllm_patch"]
