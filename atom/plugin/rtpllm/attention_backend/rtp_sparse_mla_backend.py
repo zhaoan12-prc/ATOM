91eef2df50513dc80b883f74aa526f603ac3be2c
Zhao An <zhao.an@amd.com>
2026-05-31 15:00:55 +0000

    fix: long context acc issue

--- atom/plugin/rtpllm/attention_backend/rtp_sparse_mla_backend.py
+++ atom/plugin/rtpllm/attention_backend/rtp_sparse_mla_backend.py
@@ -787,6 +787,15 @@ class RTPSparseMlaBackend:
         }
 
     @staticmethod
+    def _strict_sparse() -> bool:
+        return os.getenv("ATOM_RTP_SPARSE_MLA_STRICT", "0").strip().lower() in {
+            "1",
+            "true",
+            "yes",
+            "on",
+        }
+
+    @staticmethod
     def _impl_accepts_positions(impl: object) -> bool:
         try:
             signature = inspect.signature(impl.forward)
@@ -870,4 +879,8 @@ class RTPSparseMlaBackend:
                 **kwargs,
             )
         except _SparseUnavailable:
-            raise
+            if self._strict_sparse():
+                raise
+            return self._dense_forward(
+                q, compressed_kv, k_pe, kv_cache, layer_id, topk_indices, positions
+            )
